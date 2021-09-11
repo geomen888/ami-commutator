2db133aa1f81c8866588534cf5bd4bb042a62740
Geo <eugene.geyman@teaminua.com>
2021-09-11 11:33:14 +0300

    remove redis

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -6,17 +6,17 @@ export default () => ({
   jwtSecret: env.get('JWT_SECRET_KEY'),
   amiPort: env.get('AMI_PORT'),
   amiHostIp: env.get('AMI_HOST_IP'),
-  redis: env.get('APP_ENV') === 'ec2'
-    ? {
-      host: env.get('REDIS_HOST'),
-    }
-    : {
-      url: env.get('REDIS_URL'),
-      name: 'ami-calls',
-      onClientReady: (client) => {
-        client.on('error', (err) => {
-          console.error('redis:error::', err);
-        })
-      },
-    }
+  // redis: env.get('APP_ENV') === 'ec2'
+  //   ? {
+  //     host: env.get('REDIS_HOST'),
+  //   }
+  //   : {
+  //     url: env.get('REDIS_URL'),
+  //     name: 'ami-calls',
+  //     onClientReady: (client) => {
+  //       client.on('error', (err) => {
+  //         console.error('redis:error::', err);
+  //     })
+  //  },
+  //  }
 });
